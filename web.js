e8a3c478b20d0342ddbaa7ed13e2cbf62858d992
Xabier Larrakoetxea <slok69@gmail.com>
2013-07-10 20:36:11 +0200

    Read from index.html file

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+
+  var resp = fs.readFileSync('index.html').toString('utf-8');
+  response.send(resp);
 });
 
 var port = process.env.PORT || 5000;
