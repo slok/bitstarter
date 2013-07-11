0cedc514f868804010318d68ecadebd8b7c3c4e3
Xabier Larrakoetxea <slok69@gmail.com>
2013-07-11 19:16:17 +0200

    Add to the Grader the ability to get urls also

--- grader.js
+++ grader.js
@@ -24,6 +24,7 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
@@ -36,16 +37,46 @@ var assertFileExists = function(infile) {
     return instr;
 };
 
+var validURL = function(url) {
+    var urlOk = true;
+    if(!urlOk) {
+        console.log("%s Isn't a valid url", url);
+        process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
+    }
+    return url;
+};
+
+
+// Cheerio utils --------
 var cheerioHtmlFile = function(htmlfile) {
-    return cheerio.load(fs.readFileSync(htmlfile));
+    return cheerioHtmlString(fs.readFileSync(htmlfile));
+};
+
+var cheerioHtmlString = function(str) {
+    return cheerio.load(str);
 };
 
 var loadChecks = function(checksfile) {
     return JSON.parse(fs.readFileSync(checksfile));
 };
 
-var checkHtmlFile = function(htmlfile, checksfile) {
-    $ = cheerioHtmlFile(htmlfile);
+
+// HTML checker utils --------
+var checkHtmlUrl= function(url, checksfile, callback) {
+    rest.get(url).on('complete', function(data) {
+      msg = cheerioHtmlString(data);
+      result = checkHtml(msg, checksfile);
+      callback(result);
+    });
+};
+
+var checkHtmlFile = function(htmlfile, checksfile, callback) {
+    msg = cheerioHtmlFile(htmlfile);
+    result = checkHtml(msg, checksfile);
+    callback(result);
+};
+
+var checkHtml = function($, checksfile) {
     var checks = loadChecks(checksfile).sort();
     var out = {};
     for(var ii in checks) {
@@ -65,10 +96,22 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <url>', 'url to index.html', clone(validURL))
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+
+
+    // Decouple the callback to a var to not repeat code in each func.
+    var myCallback = function(result){
+      console.log(JSON.stringify(result, null, 4));
+    };
+
+    if (program.url !== undefined ){
+      checkHtmlUrl(program.url, program.checks, myCallback);
+    }else{
+      checkHtmlFile(program.file, program.checks, myCallback);
+    }
+
+
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
